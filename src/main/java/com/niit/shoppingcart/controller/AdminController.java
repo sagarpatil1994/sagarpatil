183eaef9f1d918dcfd5a3a0fbb5d65ef77302427
sagar patil <sagar patil>
2016-07-27 20:03:33 +0530

    7th commit

---
+++ src/main/java/com/niit/shoppingcart/controller/AdminController.java
@@ -0,0 +1,65 @@
+package com.niit.shoppingcart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.shoppingcartbackend.dao.CategoryDAO;
+import com.niit.shoppingcartbackend.dao.ProductDAO;
+import com.niit.shoppingcartbackend.dao.SupplierDAO;
+import com.niit.shoppingcartbackend.model.Category;
+import com.niit.shoppingcartbackend.model.Product;
+import com.niit.shoppingcartbackend.model.Supplier;
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private Product product;
+
+	@Autowired
+	private Supplier supplier;
+
+	@Autowired
+	private Category category;
+	
+	@Autowired
+	private SupplierDAO supplierDAO;
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
+	
+	
+	@Autowired
+	private ProductDAO productDAO;
+
+	
+	@RequestMapping("/manageCategories")
+	public ModelAndView categories() {
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("category", category);
+		mv.addObject("isAdminClickedCategories", "true");
+		mv.addObject("categoryList", categoryDAO.list());
+		return mv;
+	}
+
+	@RequestMapping("/manageProducts")
+	public ModelAndView suppliers() {
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("product", product);
+		mv.addObject("isAdminClickedProducts", "true");
+		mv.addObject("productList", productDAO.list());
+		return mv;
+	}
+
+	@RequestMapping("/manageSuppliers")
+	public ModelAndView products() {
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("supplier", supplier);
+		mv.addObject("isAdminClickedSuppliers", "true");
+		mv.addObject("supplierList", supplierDAO.list());
+		return mv;
+	}
+
+}
